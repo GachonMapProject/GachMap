771ebbdde3bb85ebfc5940dc24359f1592edcc29
wwj0313 <dndwnsla@gmail.com>
2024-04-15 14:17:43 +0900

    Initial Commit

---
+++ GachMap/GachMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GachMapApp.swift
+//  GachMap
+//
+//  Created by 원웅주 on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct GachMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
