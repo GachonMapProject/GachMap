e4ece34146db4b3cf124c6ab0cf5987cbe1f8391
suhyeon lee <tngus0673@naver.com>
2024-04-28 22:22:15 +0900

    경로 boxNode 중간 화살표 노드 생성

--- GachMap/AR/ARMainView.swift
+++ GachMap/AR/ARMainView.swift
@@ -22,7 +22,7 @@ struct ARMainView: View {
     @State var rotationList: [Rotation]? = nil      // 중간 노드의 회전과 거리를 나타낸 배열
     
     let timer = MyTimer()
-    let path = Path().ITtoGlobal
+    let path = Path().homeToAI
     
     var body: some View {
         if coreLocation.location != nil{
