0a1a39766c07b0bc14770ac4e73108dfe3a18fa9
suhyeon lee <tngus0673@naver.com>
2024-04-30 16:46:52 +0900

    무당이 경로 없을 시 버튼 비활성화

--- GachMap/Views/PathTimeTestView.swift
+++ GachMap/Views/PathTimeTestView.swift
@@ -9,37 +9,47 @@ import SwiftUI
 
 struct PathTimeTestView: View {
     @State var selectedNum = 0
+    let arr = [["최적 경로", "33", "true"], ["무당이 경로", "0", "true"], ["최단 경로", "33", "true"]]
     var body: some View {
         ScrollView(.horizontal){
             ZStack(){
                 LazyHStack{
-                    PathTimeView(pathName: "최적 경로", time: "33", isLogin: true, num: 0, selectedNum: $selectedNum)
-                        .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                            content
-                                .opacity(phase.isIdentity ? 1.0 : 0.8)
-                        }
-                        .padding(.trailing, 10)
-                        .onTapGesture {
-                            selectedNum = 0
-                        }
-                    PathTimeView(pathName: "무당이 경로", time: nil, isLogin: true, num: 1, selectedNum: $selectedNum)
-                        .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                            content
-                                .opacity(phase.isIdentity ? 1.0 : 0.8)
-                                
-                        }
-                        .padding(.trailing, 10)
-                        .onTapGesture {
-                            selectedNum = 1
-                        }
-                    PathTimeView(pathName: "최단 경로", time: "3", isLogin: true, num: 2, selectedNum: $selectedNum)
-                        .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                            content
-                                .opacity(phase.isIdentity ? 1.0 : 0.8)
-                        }
-                        .onTapGesture {
-                            selectedNum = 2
-                        }
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        PathTimeView(pathName: "최적 경로", time: arr[0][1], isLogin: true, num: 0, selectedNum: $selectedNum)
+                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+                            }
+                            .padding(.trailing, 10)
+                            .onTapGesture {
+                                selectedNum = 0
+                            }
+                    })
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        PathTimeView(pathName: "무당이 경로", time: arr[1][1] == "0" ? nil : arr[1][1], isLogin: true, num: 1, selectedNum: $selectedNum)
+                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+                                    
+                            }
+                            .padding(.trailing, 10)
+                            .onTapGesture {
+                                selectedNum = 1
+                            }
+                    })
+                    .disabled(arr[1][1] == "0")
+                    
+                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                        PathTimeView(pathName: "최단 경로", time: "3", isLogin: true, num: 2, selectedNum: $selectedNum)
+                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                content
+                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+                            }
+                            .onTapGesture {
+                                selectedNum = 2
+                            }
+                    })
+
                 } // end of LazyStack
                 .padding(EdgeInsets(top: 0, leading: 30, bottom: 30, trailing: 30))
                 .scrollTargetLayout()
