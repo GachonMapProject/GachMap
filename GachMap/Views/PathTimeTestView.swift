7a4914afb88e4ca018a8bafe78a314a6e021859e
suhyeon lee <tngus0673@naver.com>
2024-04-30 17:05:02 +0900

    pathTimeTestView 재사용

--- GachMap/Views/PathTimeTestView.swift
+++ GachMap/Views/PathTimeTestView.swift
@@ -7,48 +7,37 @@
 
 import SwiftUI
 
+struct pathTime : Identifiable {
+    let id = UUID()
+    let pathName : String
+    let time : String?
+    let isLogin : Bool
+}
+
 struct PathTimeTestView: View {
     @State var selectedNum = 0
-    let arr = [["최적 경로", "33", "true"], ["무당이 경로", "0", "true"], ["최단 경로", "33", "true"]]
+    let test = [pathTime(pathName: "최적 경로", time: "33", isLogin: true),
+                pathTime(pathName: "무당이 경로", time: nil, isLogin: true),
+                pathTime(pathName: "최단 경로", time: "3", isLogin: true)
+            ]
     var body: some View {
         ScrollView(.horizontal){
             ZStack(){
                 LazyHStack{
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        PathTimeView(pathName: "최적 경로", time: arr[0][1], isLogin: true, num: 0, selectedNum: $selectedNum)
-                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                                content
-                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
-                            }
-                            .padding(.trailing, 10)
-                            .onTapGesture {
-                                selectedNum = 0
-                            }
-                    })
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        PathTimeView(pathName: "무당이 경로", time: arr[1][1] == "0" ? nil : arr[1][1], isLogin: true, num: 1, selectedNum: $selectedNum)
-                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                                content
-                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
-                                    
-                            }
-                            .padding(.trailing, 10)
-                            .onTapGesture {
-                                selectedNum = 1
-                            }
-                    })
-                    .disabled(arr[1][1] == "0")
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
-                        PathTimeView(pathName: "최단 경로", time: "3", isLogin: true, num: 2, selectedNum: $selectedNum)
-                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
-                                content
-                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
-                            }
-                            .onTapGesture {
-                                selectedNum = 2
-                            }
-                    })
+                    ForEach(0..<3){ index in
+                        Button(action: {}, label: {
+                            PathTimeView(pathName: test[index].pathName, time:  test[index].time , isLogin: true, num: index, selectedNum: $selectedNum)
+                                .scrollTransition(.animated, axis: .horizontal) { content, phase in
+                                    content
+                                        .opacity(phase.isIdentity ? 1.0 : 0.8)
+                                }
+                                .padding(.trailing, 10)
+                                .onTapGesture {
+                                    selectedNum = index
+                                }
+                        })
+                        .disabled(test[index].time == nil)
+                    }
 
                 } // end of LazyStack
                 .padding(EdgeInsets(top: 0, leading: 30, bottom: 30, trailing: 30))
@@ -64,3 +53,41 @@ struct PathTimeTestView: View {
 #Preview {
     PathTimeTestView()
 }
+
+
+//
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                        PathTimeView(pathName: "최적 경로", time: arr[0][1], isLogin: true, num: 0, selectedNum: $selectedNum)
+//                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+//                                content
+//                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+//                            }
+//                            .padding(.trailing, 10)
+//                            .onTapGesture {
+//                                selectedNum = 0
+//                            }
+//                    })
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                        PathTimeView(pathName: "무당이 경로", time: arr[1][1] == "0" ? nil : arr[1][1], isLogin: true, num: 1, selectedNum: $selectedNum)
+//                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+//                                content
+//                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+//
+//                            }
+//                            .padding(.trailing, 10)
+//                            .onTapGesture {
+//                                selectedNum = 1
+//                            }
+//                    })
+//                    .disabled(arr[1][1] == "0")
+//
+//                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+//                        PathTimeView(pathName: "최단 경로", time: "3", isLogin: true, num: 2, selectedNum: $selectedNum)
+//                            .scrollTransition(.animated, axis: .horizontal) { content, phase in
+//                                content
+//                                    .opacity(phase.isIdentity ? 1.0 : 0.8)
+//                            }
+//                            .onTapGesture {
+//                                selectedNum = 2
+//                            }
+//                    })
